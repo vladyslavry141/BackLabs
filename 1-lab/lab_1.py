95617c567839ec6193df6a2c0bfa3c4afe359ccd
Vladyslav Rybak <vladyslavry141@gmail.com>
2021-02-21 22:11:01 +0200

    Add comments

--- 1-lab/lab_1.py
+++ 1-lab/lab_1.py
@@ -2,7 +2,8 @@ import math
 import matplotlib.pyplot as plt
 import numpy as np
 
-
+# Return the values ​​for our function according to the variant
+# None value is needed to create gaps in the plot
 def my_func(x):
     if x is None:
         return None
@@ -12,7 +13,10 @@ def my_func(x):
     else:
         return x / math.tan(x)
 
-
+# This is modified function numpy.arange 
+# which return evenly spaced values within a given interval
+# and None when we have breake in plot
+#  (for our function when the number is a multiple of pi)
 def special_range(start, end, step):
     if start >= end:
         raise Exception("End should be bigger then start")
@@ -33,16 +37,16 @@ def special_range(start, end, step):
         i += step
     return result
 
-
-def print_table (x, y):
+# Create tabulagram and print it in console line
+def print_table(x, y):
     print('{:<30} {:<30}'.format('x', 'f(x)'))
     for i in range(len(x)):
         if(x[i] is None):
             continue	
         print('{:<30} {:<30}'.format(x[i], y[i]))	
 
-
-def spec_point_for_X (start, end):
+# Calculates points which intersect the axis X
+def spec_point_for_X(start, end):
     points = []
     pi2_point = math.ceil(start / (np.pi/2)) * (np.pi/2)
     if pi2_point % np.pi == 0:
@@ -52,8 +56,8 @@ def spec_point_for_X (start, end):
         pi2_point += np.pi	
     return points
 
-
-def print_spec_points_for_X (spec_for_X):
+# Print point which intersect the axis X
+def print_spec_points_for_X(spec_for_X):
     if len(spec_for_X) > 0:
         print("Special points for X:")
         print('{:<30} {:<30}'.format('x', 'f(x)'))
@@ -62,7 +66,7 @@ def print_spec_points_for_X (spec_for_X):
     else:
         print("No special point for X")
 
-
+# Print point which intersect the axis X
 def print_spec_points_for_Y(start, end):
     if start <= 0 and end > 0:
         print("Special points for Y:")	
@@ -71,17 +75,23 @@ def print_spec_points_for_Y(start, end):
     else:
         print("No special point for Y")		
 				
-		
+# Get the values for interval and step	
 start = float(input("Enter the beginning of the interval    "))
 end = float(input("Enter the end of the interval    "))
 step = float(input("Enter a tab step    "))
+
+# Create the lists of values 
 x = special_range(start, end, step)
 y = list(map(my_func, x))
-high = abs(start - end)
+
+# Print required information
 print_table(x, y)
 spec_for_X = spec_point_for_X(start, end)
 print_spec_points_for_X(spec_for_X)
 print_spec_points_for_Y(start, end)
+
+# Bild the plot
+high = abs(start - end)
 plt.axis([start, end, - high/2 - 3, high/2 + 3])
 plt.grid(True)
 plt.plot(x, y)
