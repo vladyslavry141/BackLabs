bf16b4d0b90e8c5ea90d16f22bb86a1bacbf29cd
Vladyslav Rybak <vladyslavry141@gmail.com>
2021-03-03 16:58:16 +0200

    Some fix

--- 2-lab/lab.py
+++ 2-lab/lab.py
@@ -1,24 +1,30 @@
 import re
+import keyword
 
-def find_words(indef):
-	p = re.compile(r'\b[a-z]{3}\b')
-	list_words = p.findall(indef)
-	result = ", ".join(result)
+def find_words(user_input):
+	find_words_re = re.compile(r'\b[^\W\d][\w]{0,78}\b')
+	finded_indef = find_words_re.findall(user_input)
+	checked = filter(lambda word: word not in keyword.kwlist, list_words)
+	result = ", ".join(checked)
 	print(result)
 
 
-def check_word(indef):
-	p = re.compile(r'\b[a-z]{3}\b')
-	match = p.fullmatch(indef)
-	print("True" if match else 'False') 
+def check_word(user_input):
+	p = re.compile(r'\b[^\W\d][\w]{0,78}\b')
+	match = p.fullmatch(user_input)
+	if match is None:
+		print(False)
+	else:
+		print(match not in keyword.kwlist)
 
 
-
-
-variant = input("Press 1 to check one identifier or 2 to find the list of identifiers   ")
-indeficators = input("enter the string   ")
+variant = input("""Press:
+(1) to check one identifier
+(2) to find the list of identifiers: """)
+user_input = input("Enter the string: ")
 if(variant == '1'):
-	check_word(indeficators)
+	check_word(user_input)
 elif(variant == '2'):
-	find_words(indeficators)
-
+	find_words(user_input)
+else:
+	print("Wrong input")
